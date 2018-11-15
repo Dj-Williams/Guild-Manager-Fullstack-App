e334ed32bc9b6f030c9ec513e0d1c9e6efbe9a46
CB_ATL_110_David_Williams <cb_atl_110_david_williams@MacBook-Air-87.local>
2018-11-15 17:52:54 -0500

    edited app.js for client. Phase 1 complete. Phase 2 db folder next

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <h1>WELCOME TO THE 1st PHASE OF YOUR APP! REACT AND EXPRESS ARE CONNECTED!</h1>
+      <h3>Next we're going to gut all this other stuff outta here, and get into the nitty gritty of making that custom feel...feel real!</h3>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
